8b97f8fd3b059add3bc1918bfd9e07481c9be92a
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2023-01-26 16:07:37 +0900

    New: Translator.js

---
+++ Translator.js
@@ -0,0 +1,42 @@
+
+/** op-webpack-js:/Translator.js
+ *
+ * @created   2023-01-23
+ * @version   1.0
+ * @package   op-app-skeleton-2020-nep
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/* <?php if( OP()->Config('translate')['execute'] ?? null ): ?> */
+
+//	...
+(function(){
+	//	...
+	if(!$OP ){
+		$OP = {};
+	}
+
+	//	...
+	$OP['Translator'] = function(){
+		//	...
+		['i18n','translate'].forEach(key => {
+			//	...
+			document.querySelectorAll(`[data-${key}="true"]`).forEach(html => {
+				//	...
+				$OP['Translate'](html.innerHTML, function(result){
+					html.innerHTML    =  result;
+					html.dataset[key] = 'false';
+				});
+			});
+		});
+	};
+})();
+
+//	...
+document.addEventListener('DOMContentLoaded', () => {
+	//	...
+	$OP['Translator']();
+});
+
+/* <?php endif; ?> */
