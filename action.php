e02bd227aabe97d0b1454cafd377e166e07ab49c
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2023-03-09 23:33:54 +0900

    Fix: action.php | v2.0 - Automatically entry.

--- action.php
+++ action.php
@@ -1,25 +1,47 @@
 <?php
 /** op-webpack-js:/action.php
  *
+ * v2.0 is automatically entry.
+ *
  * @created   2018-04-12
- * @version   1.0
+ * @version   2.0
  * @package   op-webpack-js
  * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
  * @copyright Tomoaki Nagahara All right reserved.
  */
-return [
-	__DIR__.'/op',
-	__DIR__.'/d',
-	__DIR__.'/env',
-	__DIR__.'/path',
-	__DIR__.'/args',
-	__DIR__.'/url',
-	__DIR__.'/ajax',
-	__DIR__.'/date',
-	__DIR__.'/html',
-	__DIR__.'/form-disabler',
-	__DIR__.'/Hash',
-	__DIR__.'/Translate',
-	__DIR__.'/Translator',
-	__DIR__.'/Translate_Language',
-];
+
+/** Declare strict
+ *
+ */
+declare(strict_types=1);
+
+/** namespace
+ *
+ */
+namespace OP;
+
+//	...
+$return = [];
+
+//	...
+foreach( glob(__DIR__.'/*.js') as $file ){
+	//	...
+	if( $file === 'index.js' ){
+		//	Why?
+		continue;
+	}
+
+	//	...
+	if( $file[0] === '.' or $file[0] === '_' ){
+		continue;
+	}
+
+	//	...
+	$name = substr($file, 0, -3);
+
+	//	...
+	$return[] = $name;
+}
+
+//	...
+return $return;
