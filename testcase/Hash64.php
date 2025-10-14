40134da67faf0123a5251058fa1b235dbafe13b6
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-10-14 21:27:25 +0900

    Add: Hash64.js | Hash64()

---
+++ testcase/Hash64.php
@@ -0,0 +1,23 @@
+<?php
+/** op-webpack-js:/testcase/Hash64.js
+ *
+ * @created    2025-10-12
+ * @version    1.0
+ * @package    op-webpack-js
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+//	...
+OP()->Unit()->WebPack()->Auto('asset:/webpack/js/Hash64.js');
+?>
+<h1>Usage</h1>
+<ol>
+	<li>Open the developer console in your browser.</li>
+	<li>
+		Enter the following code into the Developer Console:
+<pre><code>
+console.log( Hash64("Hello new world 🌏") );
+</code></pre>
+	</li>
+</ol>
